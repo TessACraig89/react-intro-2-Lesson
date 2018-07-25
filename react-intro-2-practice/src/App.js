6c9a69d7b1affeb5f2edb1e03510813b26483066
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-25 18:07:31 -0500

    changes App.js componenet to be a basic Hello component

---
+++ react-intro-2-practice/src/App.js
@@ -0,0 +1,15 @@
+// bring in React and Component instance from react
+import React, {Component} from 'react'
+
+// define our Hello component
+class Hello extends Component {
+  // what should the component render
+  render () {
+    // Make sure to return some UI
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
+}
+
+export default Hello
