863ed000a36134a2a5d9170403ace16b88e324ca
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-25 19:41:48 -0500

    adds button with onClick attribute thats value is an anonymous function that invokes handleClick

--- react-intro-2-practice/src/App.js
+++ react-intro-2-practice/src/App.js
@@ -14,7 +14,7 @@ class Hello extends Component {
       counter: 0 // initialize this.state.counter to be 0
     }
   }
-  //trigger event 
+  //trigger event
   handleClick (e) {
     // setState is inherited from the Component class
     this.setState({
@@ -24,10 +24,14 @@ class Hello extends Component {
   render () {
     // Make sure to return some UI
     /*{this.props.name}reference to that data via as a property on the props object*/
+    // We use an attribute called onClick to define what happens when we click this button. As it's value, we're passing in an anonymous function that invokes handleClick, a function defined on this component.
     return (
       <div>
         <h1>Hello {this.props.name}</h1>
         <p>You are {this.props.age} years old</p>
+        <p>The initial count is {this.state.counter}
+       </p>
+       <button onClick={(e) => this.handleClick(e)}>click me!</button>
       </div>
     )
   }
