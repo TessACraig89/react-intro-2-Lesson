b60d92972e601d58d38a336f709407cb2460920c
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-25 19:19:51 -0500

    implements state by incorporating a counter in Hello component by calling constructor function and defining counters initial state

--- react-intro-2-practice/src/App.js
+++ react-intro-2-practice/src/App.js
@@ -3,7 +3,17 @@ import React, {Component} from 'react'
 
 // define our Hello component
 class Hello extends Component {
+  // when our component is initialized,
+  // our constructor function is called
   // what should the component render
+  constructor (props) {
+    // make call to parent class' (Component) constructor
+    super()
+    // define an initial state
+    this.state = {
+      counter: 0 // initialize this.state.counter to be 0
+    }
+  }
   render () {
     // Make sure to return some UI
     /*{this.props.name}reference to that data via as a property on the props object*/
